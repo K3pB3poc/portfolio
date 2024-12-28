149c39a1c0143d8e765645e7f4ddc19930df9d0b
K3pB3poc <K3pB3poc@gmail.com>
2024-12-28 18:38:53 -0300

    tratando de agregar la API

--- script.js
+++ script.js
@@ -17,3 +17,71 @@ btnPopUpClose.addEventListener('click', function() {
 btnSubmit.addEventListener('click', function() {
     overlay.classList.remove('active');
 });
+
+// const cargartoken = async() => {
+//     try {
+//         const respuesta = await fetch ('https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&symbol=IBM&interval=5min&apikey=7G494HY3DOJIWLHU');
+
+//         console.log(respuesta);
+//         if(respuesta.status ===200){
+//             console.log(respuesta.status);
+//             const datos = await respuesta.json();
+
+//             let stocks = '';
+//             console.log(datos);
+//             datos.top_gainers.forEach(top_gainer => {
+//                 stocks = stocks + '<H1>ticker</H1>';
+//                 console.log(top_gainer.ticker);
+//             });
+
+//             document.getElementById('stocks').innerHTML = stocks;
+            
+//         }else if(respuesta.status === 401){
+//             console.log('Verificar clave API');
+
+//         }else if(respuesta.status === 404){
+//             console.log('El dato buscado no se encuentra');
+
+//         }else 
+//             console.log('Error no indicado por la API')
+            
+            
+            
+        
+//     }catch(error){
+//         console.log(error);
+//     }
+        
+       
+// }
+
+// cargartoken();
+
+
+const cargartoken = async() => {
+    try {
+        const respuesta = await fetch ('https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&symbol=IBM&interval=5min&apikey=7G494HY3DOJIWLHU');
+
+        console.log(respuesta);
+        if(respuesta.status ===200){
+            console.log(respuesta.status);
+            const datos = await respuesta.json();
+
+            let stocks = '';
+            console.log(datos);
+            datos.top_gainers.forEach(top_gainer => {
+                stocks = stocks + '<H1>ticker</H1>';
+                console.log(top_gainer.ticker);
+            });
+
+            document.getElementById('stocks').innerHTML = stocks;
+            
+        }else if(respuesta.status === 401){
+            console.log('Verificar clave API');
+
+        }else if(respuesta.status === 404){
+            console.log('El dato buscado no se encuentra');
+
+        }else 
+            console.log('Error no indicado por la API')
+
